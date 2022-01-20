78c0e2f13227fbaa103be6115fd044dfc5350032
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-01-20 14:29:19 -0600

    Coding Exercise 35 of Java programming Masterclass

---
+++ Coding_Exercise_35/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(3.75);
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+
+        System.out.println(cylinder.getVolume());
+        System.out.println(cylinder.getArea());
+
+
+    }
+}
