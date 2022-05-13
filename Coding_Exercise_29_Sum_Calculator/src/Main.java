2d4635e693566f84ed3c5416296779dba3740d2d
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-05-13 12:23:29 -0500

    Coding exercise covering encapsulation principles

---
+++ Coding_Exercise_29_Sum_Calculator/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+            SimpleCalculator calculator = new SimpleCalculator();
+
+            calculator.setFirstNumber(5);
+            calculator.setSecondNumber(4);
+
+        System.out.println(calculator.getAdditionResult());
+    }
+}
