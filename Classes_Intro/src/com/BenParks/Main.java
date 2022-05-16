9bb9b01fb9a024a74089fbf9126df05504e300c0
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-05-16 11:14:45 -0500

    Wall Area with constructors problem

--- Classes_Intro/src/com/BenParks/Main.java
+++ Classes_Intro/src/com/BenParks/Main.java
@@ -1,6 +1,17 @@
+package com.BenParks;
+
 public class Main {
 
     public static void main(String[] args) {
+     
+        Car porche = new Car();
 
+        Car holden = new Car();
+
+        porche.setModel("Carrera");
+        System.out.println(porche.getModel());
+
+
+        
     }
 }
